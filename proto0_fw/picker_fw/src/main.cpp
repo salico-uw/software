220833826e64a1bd03b0cd704ae0a7696647d32d
Daniel Ye <dzyye@uwaterloo.ca>
2024-10-10 00:34:32 -0400

    add rotary encoder

--- proto0_fw/picker_fw/src/main.cpp
+++ proto0_fw/picker_fw/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <SimpleFOC.h>
-// #include <rotary.h>
+#include <rotary.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
 
 /* Using STMF401RE Nucleo with IHM08M1 motor sheild,
 * AS5600 I2C angle sensor
+* rotary encoder with pushbutton for mode control
 */
 
 #define PWM_MODE 6 // For 3pwm make sure the pwms are tied together
@@ -36,27 +39,48 @@
 #define WL PB1
 
 #define STATUS_LED PC6
-#define POT_PIN PA4 // pot ranges 740 - 785
-#define ENCODER_PIN1 PA5
-#define ENCODER_PIN2 PA6
-
-#define ENCODER_SPEED_INCREMENT 1.0f // rad/s
-#define MAX_SPEED 10.0f // rad/s
+#define POT_PIN PA4 // (UNUSED) onboard pot ranges 740 - 785
+#define ENCODER_PIN1 PC6
+#define ENCODER_PIN2 PC8
+#define ENCODER_BUTTON_PIN PA11
+#define SPEED_INCREMENT 1.0f // rad/s
+#define MAX_SPEED 20.0f // rad/s
+#define CURRENT_INCREMENT 0.5f // rad/s
+#define MAX_CURRENT 15.0f // rad/s
 
 #define SUPPLY_VOLTAGE (12U)
-#define BUTTON_DEBOUNCE_MS (300U) // ms
+#define BUTTON_DEBOUNCE_MS (50U) // ms
+
+#define SCREEN_ADDRESS 0x3C /// 0x3C for 128x32
+
 #if PWM_MODE == 6
 BLDCDriver6PWM driver = BLDCDriver6PWM(UH, UL, VH, VL, WH, WL);
 #elif PWM_MODE == 3
 BLDCDriver3PWM driver = BLDCDriver3PWM(UH, VH, WH);
 #endif // PWM_MODE
+
+#if CALIBRATION_MODE
+// Need to use voltage limit mode for calibration, if specifying phase resistance it will use current limit
+BLDCMotor motor = BLDCMotor(MOTOR_POLE_PAIRS);
+#else
 BLDCMotor motor = BLDCMotor(MOTOR_POLE_PAIRS, MOTOR_PHASE_RESISTANCE);
+#endif // CALIBRATION_MODE
 MagneticSensorI2C angleSensor = MagneticSensorI2C(AS5600_I2C);
 
-// Rotary encoder = Rotary(ENCODER_PIN1, ENCODER_PIN2);
+Rotary encoder = Rotary(ENCODER_PIN1, ENCODER_PIN2);
+// static TwoWire Wire2 = TwoWire(PB3, PB10);
+// Adafruit_SSD1306 display(128, 32, &Wire2, -1);
 
 Commander commander = Commander(Serial);
 void onMotor(char* cmd){ commander.motor(&motor,cmd); }
+
+enum Mode_E {
+  OFF_MODE,
+  SPEED_MODE,
+  CURRENT_MODE,
+};
+
+Mode_E menuMode = OFF_MODE;
 
 bool prevButton = true; // active low
 uint32_t last_millis = 0U;
@@ -67,7 +91,7 @@ bool wasButtonPressed(){
   {
     last_millis = millis();
   }
-  bool currButton = digitalRead(USER_BTN);
+  bool currButton = digitalRead(ENCODER_BUTTON_PIN);
   if(currButton == false && prevButton == true && (millis() - last_millis) > BUTTON_DEBOUNCE_MS)
   {
     pressed = true;
@@ -78,22 +102,63 @@ bool wasButtonPressed(){
 }
 
 double speed_target = 0.0f; // rad/s
-// void check_encoder() {
-//   // unsigned char result = encoder.process();
-//   if (result == DIR_CW && speed_target <= MAX_SPEED) {
-//     speed_target += ENCODER_SPEED_INCREMENT;
-//     Serial.println(speed_target);
-//   } else if (result == DIR_CCW && speed_target >= -MAX_SPEED) {
-//     speed_target -= ENCODER_SPEED_INCREMENT;
-//     Serial.println(speed_target);
-//   }
-// }
+double current_limit = 5.0f; // amps
+uint32_t last_encoder_millis = 0U;
+void check_encoder() {
+  if (millis() < last_encoder_millis)
+  {
+    last_encoder_millis = millis();
+  }
+
+  double useless_number = 0.0f; // theres gotta be a better way ill fix this later
+  double mode_increment = SPEED_INCREMENT;
+  double upper_limit = MAX_SPEED;
+  double lower_limit = -MAX_SPEED;
+  double * target = &useless_number;
+
+  switch(menuMode) {
+    case OFF_MODE:
+      target = &useless_number;
+      break;
+    case SPEED_MODE:
+      mode_increment = SPEED_INCREMENT;
+      upper_limit = MAX_SPEED;
+      lower_limit = -MAX_SPEED;
+      target = &speed_target;
+      break;
+    case CURRENT_MODE:
+      mode_increment = CURRENT_INCREMENT;
+      upper_limit = MAX_CURRENT;
+      lower_limit = 0.0f;
+      target = &current_limit;
+      break;
+  }
+
+  unsigned char result = encoder.process();
+  if ((result == DIR_CW) && ((millis() - last_encoder_millis) > BUTTON_DEBOUNCE_MS)) {
+    *target += mode_increment;
+    last_encoder_millis = millis();
+  } else if ((result == DIR_CCW) && ((millis() - last_encoder_millis) > BUTTON_DEBOUNCE_MS)) {
+    *target -= mode_increment;
+    last_encoder_millis = millis();
+  }
+  // Saturate speed
+  if(*target > upper_limit)
+  {
+    *target = upper_limit;
+  } else if (*target < lower_limit) {
+    *target = lower_limit;
+  }
+
+}
 
 void setup() {
-  pinMode(USER_BTN, INPUT);
+  pinMode(ENCODER_BUTTON_PIN, INPUT_PULLUP);
   pinMode(POT_PIN, INPUT); // Pot
-  // attachInterrupt(ENCODER_PIN1, check_encoder, CHANGE);
-  // attachInterrupt(ENCODER_PIN2, check_encoder, CHANGE);
+  pinMode(ENCODER_PIN1, INPUT_PULLUP);
+  pinMode(ENCODER_PIN2, INPUT_PULLUP);
+  attachInterrupt(ENCODER_PIN1, check_encoder, CHANGE);
+  attachInterrupt(ENCODER_PIN2, check_encoder, CHANGE);
   Serial.begin(115200);
   while(!Serial) {}
   _delay(1000);
@@ -114,7 +179,7 @@ void setup() {
 
   motor.velocity_limit = 100;
   motor.controller = MotionControlType::velocity;
-  motor.current_limit = 15;
+  motor.current_limit = current_limit;
 
   motor.PID_velocity.P = 0.2;
   motor.PID_velocity.I = 5.0;
@@ -144,38 +209,28 @@ void loop() {
 
   if (wasButtonPressed())
   {
-    if(motor.enabled == false) {
-      motor.enable();
+    switch(menuMode) {
+      case OFF_MODE:
+        menuMode = SPEED_MODE;
+        motor.enable();
+        break;
+      case SPEED_MODE:
+        menuMode = CURRENT_MODE;
+        break;
+      case CURRENT_MODE:
+        menuMode = SPEED_MODE;
+        break;
     }
-    else {
-      motor.disable();
-    }
-      // double max_speed = 20; // rad/s
-      // speed_target = (analogRead(POT_PIN) - 740)/45.0*(max_speed);
-      // if (speed_target > max_speed) {
-      //   speed_target = max_speed;
-      // }
-      // else if (speed_target < -max_speed) {
-      //   speed_target = -max_speed;
-      // }
-      // speed_target -= max_speed/2.0; // to get +-velocities
-    // }
   }
-  
-  double max_current = 15; // A
-  double current_limit = (analogRead(POT_PIN) - 740)/45.0*(max_current);
-  if (current_limit > max_current) {
-    current_limit = max_current;
-  }
-  else if (current_limit < 0) {
-    current_limit = 0;
-  }
+
   motor.current_limit = current_limit;
   motor.PID_velocity.limit = motor.current_limit;
 
   if(millis() % 10 == 0) // don't print too fast
   {
-    Serial.print("angle: ");
+    Serial.print("mode: ");
+    Serial.print(menuMode);
+    Serial.print(" angle: ");
     Serial.print(angleSensor.getSensorAngle());
     Serial.print(" speed: ");
     Serial.print(angleSensor.getVelocity()*motor.sensor_direction);
@@ -184,5 +239,5 @@ void loop() {
     Serial.print(" limit: ");
     Serial.println(motor.current_limit);
   }
-  motor.move(8);
+  motor.move(speed_target);
 }
