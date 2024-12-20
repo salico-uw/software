c2ab6005b1f97c270791d4b994054cff48cbe32a
Daniel Ye <dzyye@uwaterloo.ca>
2024-12-19 21:38:06 -0500

    new motors and driver tested

--- proto1_fw/src/logger.cpp
+++ proto1_fw/src/logger.cpp
@@ -5,7 +5,7 @@
 #include "logger.h"
 #include "display.h"
 
-#define TASK_PERIOD_MS 1500U
+#define TASK_PERIOD_MS 1000U
 
 static void TaskLogger(void *pvParameters)
 {
@@ -24,33 +24,39 @@ static void TaskLogger(void *pvParameters)
 		float speedTarget = getRollerMotorSpeedTarget();
 		float motorCurrent = getRollerMotorCurrent();
 		float currentLimit = getRollerMotorCurrentLimit();
-		// Serial.print("mode: ");
-		// Serial.print(state);
-		// Serial.print(" angle: ");
-		// Serial.print(getRollerMotorAngle());
-		// Serial.print(" speed: ");
-		// Serial.print(motorSpeed);
-		// Serial.print(" target speed: ");
-		// Serial.print(speedTarget);
-		// Serial.print(" current: ");
-		// Serial.print(motorCurrent);
-		// Serial.print(" limit: ");
-		// Serial.println(currentLimit);
+		Serial.print("mode: ");
+		Serial.print(state);
+		Serial.print(" angle: ");
+		Serial.print(getRollerMotorAngle());
+		Serial.print(" speed: ");
+		Serial.print(motorSpeed);
+		Serial.print(" target speed: ");
+		Serial.print(speedTarget);
+		Serial.print(" current: ");
+		Serial.print(motorCurrent);
+		Serial.print(" limit: ");
+		Serial.println(currentLimit);
+		// Serial.print("Hall A: ");
+		// Serial.print(analogRead(PB3));
+		// Serial.print("B: ");
+		// Serial.print(analogRead(PB4));
+		// Serial.print("C: ");
+		// Serial.println(analogRead(PB5));
 
-		dp_clear();
-		double value = 0.0f;
-		double target = 0.0f;
-		if(state == OFF_STATE || state == SPEED_STATE) {
-		  value = motorSpeed;
-		  target = speedTarget;
-		} else if(state == CURRENT_STATE) {
-		  value = motorCurrent;
-		  target = currentLimit;
-		}
-		dp_draw_num(value, 0);
-		dp_draw_num(target, 1);
-		dp_draw_state(state);
-		dp_send();
+		// dp_clear();
+		// double value = 0.0f;
+		// double target = 0.0f;
+		// if(state == OFF_STATE || state == SPEED_STATE) {
+		//   value = motorSpeed;
+		//   target = speedTarget;
+		// } else if(state == CURRENT_STATE) {
+		//   value = motorCurrent;
+		//   target = currentLimit;
+		// }
+		// dp_draw_num(value, 0);
+		// dp_draw_num(target, 1);
+		// dp_draw_state(state);
+		// dp_send();
         vTaskDelay(xDelay);
     }
 }
--- proto1_fw/src/rollerMotor.cpp
+++ proto1_fw/src/rollerMotor.cpp
@@ -2,6 +2,7 @@
 #include "stateMachine.h"
 #include "encoder.h"
 #include "monitor.h"
+#include <SPI.h>
 
 // *****Adjustable defines*****
 #define TASK_PERIOD_MS 1U
@@ -12,7 +13,7 @@
 #endif
 
 // Toggle for open loop for testing
-#define OPEN_LOOP true // Dont set when CALIBRATION_MODE == true
+#define OPEN_LOOP false // Dont set when CALIBRATION_MODE == true
 #if OPEN_LOOP && CALIBRATION_MODE
 #error "Cannot calibrate in open loop mode"
 #endif
@@ -20,15 +21,13 @@
 #define SPEED_INCREMENT 1.0f // rad/s
 #define MAX_SPEED 200.0f // rad/s
 #define CURRENT_INCREMENT 0.5f // rad/s
-#if OPEN_LOOP == false
 #define MAX_CURRENT 15.0f // amps
-#else
-#define MAX_CURRENT 5.0f // amps
-#endif // OPEN_LOOP == false
 
-#define SUPPLY_VOLTAGE (12U)
+#define SUPPLY_VOLTAGE (24U)
 // Define specific motor we are using
-#define MOTOR_F80
+#define MOTOR_GEARBOX
+// Define if we are using SPI for custom motor controller
+#define USE_GD_SPI true
 
 // *****Rest of defines*****
 #ifdef MOTOR_F80
@@ -49,12 +48,61 @@
 #define MOTOR_PHASE_RESISTANCE 0.15f 
 #endif // MOTOR_U8
 
+#ifdef MOTOR_GEARBOX
+#define MOTOR_KV 145U
+#define MOTOR_POLE_PAIRS 2U
+#define MOTOR_PHASE_RESISTANCE 0.35f 
+#endif // MOTOR_U8
+
 #define UH1 PA8
 #define UH2 PA7
 #define VH1 PA9
 #define VH2 PB0
 #define WH1 PA10
 #define WH2 PB1
+
+#if USE_GD_SPI
+#define READ 0b1000000000000000
+#define WRITE 0x00
+#define FAULT_STATUS_ADDR 0x0
+#define DRIVER_CONTROL_ADDR 0x2
+#define GD_HS_ADDR 0x3
+#define GD_LOW_ADDR 0x4
+#define OCP_ADDR 0x5
+#define CSA_ADDR 0x6
+#define NCS_PIN PD2
+#define DATA_MASK 0b11111111111
+SPIClass GD_SPI(PC12, PC11, PC10);
+
+void setupSPI(void)
+{
+    pinMode(NCS_PIN, OUTPUT);
+	digitalWrite(NCS_PIN, HIGH);
+	GD_SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE_1));
+}
+
+uint16_t transmitSPI(uint16_t mosi)
+{
+	Serial.println(mosi, BIN);
+	digitalWrite(NCS_PIN, LOW);
+	uint16_t rx_buffer = GD_SPI.transfer16(mosi) & DATA_MASK;
+	digitalWrite(NCS_PIN, HIGH);
+	Serial.println(rx_buffer, BIN);
+    return rx_buffer;
+}
+
+void writeSPIRegister(uint8_t addr, uint16_t data)
+{
+    uint16_t mosi = WRITE | (addr << 11U) | data;
+    transmitSPI(mosi);
+}
+
+uint16_t readSPIRegister(uint8_t addr)
+{
+    uint16_t mosi = READ | (addr << 11U);
+    return transmitSPI(mosi);
+}
+#endif // USE_GD_SPI
 
 BLDCDriver3PWM driver1 = BLDCDriver3PWM(UH1, VH1, WH1);
 BLDCDriver3PWM driver2 = BLDCDriver3PWM(UH2, VH2, WH2);
@@ -68,15 +116,14 @@ BLDCMotor motor1 = BLDCMotor(MOTOR_POLE_PAIRS, MOTOR_PHASE_RESISTANCE);
 BLDCMotor motor2 = BLDCMotor(MOTOR_POLE_PAIRS, MOTOR_PHASE_RESISTANCE);
 #endif // CALIBRATION_MODE
 
-MagneticSensorSPI angleSensor = MagneticSensorSPI(AS5047_SPI, PD2);
-SPIClass SPI_3(PC12, PC11, PC10);
+HallSensor sensor1 = HallSensor(PB3, PB4, PB5, MOTOR_POLE_PAIRS);
 
 Commander commander = Commander(Serial);
 void onMotor1(char* cmd){ commander.motor(&motor1,cmd); }
 void onMotor2(char* cmd){ commander.motor(&motor2,cmd); }
 
 double speed_target = 0.0f; // rad/s
-double current_limit = 2.0f; // amps
+double current_limit = 3.0f; // amps
 void checkEncoder() {
   State_E state = getState();
   if(state == SPEED_STATE || state == CURRENT_STATE)
@@ -119,22 +166,62 @@ static void TaskRollerMotor(void *pvParameters)
 
     const TickType_t xDelay = TASK_PERIOD_MS / portTICK_PERIOD_MS;
     // Setup
+#if USE_GD_SPI
+    setupSPI();
+
+	Serial.println("START");
+    if(readSPIRegister(FAULT_STATUS_ADDR) != 0U)
+    {
+        Serial.println("FAULTED");
+        while(1) {}
+    }    
+
+	Serial.println("driver control read");
+    readSPIRegister(DRIVER_CONTROL_ADDR);
+
+	Serial.println("driver control write 3pwm");
+    uint16_t pwm3_data = (0b01<<5U);
+    writeSPIRegister(DRIVER_CONTROL_ADDR, pwm3_data);
+
+	Serial.println("driver control read");
+    uint16_t spi_driver_config = readSPIRegister(DRIVER_CONTROL_ADDR);
+	if(spi_driver_config != pwm3_data)
+    {
+        Serial.println("3PWM NOT CONFIGURED CORRECTLY");
+        while(1) {}
+    }
+
+	Serial.println("GD high read");
+    readSPIRegister(GD_HS_ADDR);
+
+	Serial.println("GD low read");
+    readSPIRegister(GD_LOW_ADDR);
+
+	Serial.println("OCP");
+    readSPIRegister(OCP_ADDR);
+
+	Serial.println("CSA");
+    readSPIRegister(CSA_ADDR);
+
+	GD_SPI.end();
+#endif // USE_GD_SPI
+
     encoderSetup();
 
-    commander.add('M1',onMotor1,"Motor 1");
-    commander.add('M2',onMotor2,"Motor 2");
+    commander.add('M',onMotor1,"Motor 1");
+    commander.add('N',onMotor2,"Motor 2");
 
     SimpleFOCDebug::enable(&Serial);
     // Motor 1
     motor1.useMonitoring(Serial);
     driver1.voltage_power_supply = SUPPLY_VOLTAGE;
-    driver1.pwm_frequency = 10000; // Lower pwm freq (from 25kHz default) to reduce switching loss
+    driver1.pwm_frequency = 15000; // Lower pwm freq (from 25kHz default) to reduce switching loss
     driver1.init();
     motor1.linkDriver(&driver1);
 
-    angleSensor.clock_speed = 4000000; // AS5047 SPI speed can go up to 10MHz 
-    angleSensor.init(&SPI_3);
-    motor1.linkSensor(&angleSensor);
+    sensor1.pullup = Pullup::USE_INTERN;
+    sensor1.init();
+    motor1.linkSensor(&sensor1);
 
     motor1.velocity_limit = 100;
 
@@ -145,15 +232,11 @@ static void TaskRollerMotor(void *pvParameters)
     driver2.init();
     motor2.linkDriver(&driver2);
 
-    // angleSensor2.clock_speed = 4000000; // AS5047 SPI speed can go up to 10MHz 
-    // angleSensor2.init(&SPI_3);
-    motor2.linkSensor(&angleSensor);
-
     motor2.velocity_limit = 100;
 
 #if OPEN_LOOP == false
     motor1.controller = MotionControlType::velocity;
-    motor2.controller = MotionControlType::velocity;
+    motor2.controller = MotionControlType::velocity_openloop;
 #else
 	motor1.controller = MotionControlType::velocity_openloop;
     motor2.controller = MotionControlType::velocity_openloop;
@@ -162,19 +245,19 @@ static void TaskRollerMotor(void *pvParameters)
 	// Motor 1
     motor1.current_limit = current_limit;
 
-    motor1.PID_velocity.P = 0.2;
-    motor1.PID_velocity.I = 8.0;
-    motor1.PID_velocity.D = 0.0;
+    motor1.PID_velocity.P = 0.4;
+    motor1.PID_velocity.I = 1.0;
+    motor1.PID_velocity.D = 0;
     motor1.PID_velocity.output_ramp = 1000;
-    motor1.LPF_velocity.Tf = 0.01;
+    motor1.LPF_velocity.Tf = 0.4;
 
     motor1.voltage_sensor_align = 0.5;
     #if CALIBRATION_MODE
     motor1.voltage_limit = 0.5; // Set when running initFOC for CALIBRATION ONLY to be safe
     #else
     // Determined once with initFOC calibration
-    motor1.sensor_direction = Direction::CW;
-    motor1.zero_electric_angle = 6.0;
+    motor1.sensor_direction = Direction::CCW;
+    motor1.zero_electric_angle = 3.14;
     #endif // CALIBRATION_MODE
 
     motor1.KV_rating = MOTOR_KV;
@@ -186,11 +269,11 @@ static void TaskRollerMotor(void *pvParameters)
 	// Motor 2
 	motor2.current_limit = current_limit;
 
-    motor2.PID_velocity.P = 0.2;
-    motor2.PID_velocity.I = 8.0;
-    motor2.PID_velocity.D = 0.0;
+    motor2.PID_velocity.P = 0.4;
+    motor2.PID_velocity.I = 1.0;
+    motor2.PID_velocity.D = 0;
     motor2.PID_velocity.output_ramp = 1000;
-    motor2.LPF_velocity.Tf = 0.01;
+    motor2.LPF_velocity.Tf = 0.4;
 
     motor2.voltage_sensor_align = 0.5;
     #if CALIBRATION_MODE
@@ -260,7 +343,7 @@ bool getRollerMotorEnabled(void)
 
 float getRollerMotorAngle(void)
 {
-    return angleSensor.getAngle();
+    return sensor1.getAngle();
 }
 
 float getRollerMotorSpeed(void)
